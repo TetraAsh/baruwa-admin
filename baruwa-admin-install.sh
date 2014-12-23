a2b6a76a6ac8f0adf74c0c672d0c211c7a90a652
Ash <TetraAsh@users.noreply.github.com>
2014-12-23 15:11:28 +0000

    Delete baruwa-admin-install.sh

--- baruwa-admin-install.sh
+++
@@ -1,7 +0,0 @@
-# To Install on CentOS 6.5 after running the baruwa-install.com install script.
-
-
-cd /usr/sbin
-curl -O https://raw.githubusercontent.com/tetraash/baruwa-admin/master/baruwa-admin
-chmod +x baruwa-admin
-
